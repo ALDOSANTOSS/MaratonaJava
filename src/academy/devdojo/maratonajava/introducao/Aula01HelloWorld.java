1fb9d42447bd7553eb500d45f80cd45b2640f052
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-23 12:01:04 -0300

    Tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula01HelloWorld.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01HelloWorld.java
@@ -1,4 +1,6 @@
-public class HelloWorld {
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
