86c224a5e07aeff2aa93dff1877719cbad3e2515
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-23 18:01:22 -0300

    Exercico endereço

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03tiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03tiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Pedro";
+        String endereco = "Avenida Brasil";
+        double salario = 5403.23;
+        String dataRecebimentoSalario = "10/10/22";
+        System.out.println("Eu " + nome + "Morado do Endereço " + endereco + "Valor do salario " + salario + " Data de receber " + dataRecebimentoSalario);
+
+    }
+}
+
+
+
