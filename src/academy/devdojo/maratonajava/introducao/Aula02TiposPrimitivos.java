1203b6f360f975998f9c759a09d15112ab30a92e
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-23 16:39:37 -0300

    String

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -13,7 +13,18 @@ public class Aula02TiposPrimitivos {
         boolean falso = false;
         char caractere = 'M';
 
+        String texto = "Bom dia amigo";
+
         System.out.println(idade);
+        System.out.println(numeroGrande);
+        System.out.println(salarioDouble);
+        System.out.println(salarioFloat);
+        System.out.println(idadeByte);
+        System.out.println(idadeShort);
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println(caractere);
+        System.out.println(texto);
 
     }
 }
