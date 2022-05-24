b60eca2b80756dc2c1dcf6acee030a4bb667fc2f
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-24 11:10:07 -0300

    New

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04OperadoresArimeticos.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04OperadoresArimeticos {
+    public static void main(String[] args) {
+
+        // + - / *
+
+        int numero01 = 10;
+        int numero02 = 10;
+        int resultado = numero01 + numero02;
+        System.out.println("Resultado = " + resultado);
+    }
+}
+
