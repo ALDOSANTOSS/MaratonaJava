90928382ae1a50abc94d912aca20b65176b00db4
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-24 12:05:02 -0300

    Boolean

--- src/academy/devdojo/maratonajava/introducao/Aula04OperadoresArimeticos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04OperadoresArimeticos.java
@@ -9,6 +9,29 @@ public class Aula04OperadoresArimeticos {
         int numero02 = 10;
         int resultado = numero01 + numero02;
         System.out.println("Resultado = " + resultado);
+
+
+        // resto %
+
+        int resto = 21 % 2;
+        System.out.println(resto);
+
+        // <> <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10.0;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+        System.out.println(isDezIgualVinte);
+        System.out.println(isDezIgualDez);
+        System.out.println(isDezDiferenteDez);
+
+
     }
+
+
 }
 
