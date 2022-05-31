a7c3bbd7390b7055c482f563644a096821821aed
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-31 00:55:51 -0300

    Revisao

---
+++ src/academy/devdojo/maratonajava/introducao/lista/condicional/exercicio/Lista11PraticaFor04.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao.lista.condicional.exercicio;
+
+public class Lista11PraticaFor04 {
+    public static void main(String[] args) {
+        for (int numero = 98;numero <= 100;numero++){
+            System.out.println(numero);
+        }
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/lista/condicional/exercicio/Lista11PraticaWhile04.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao.lista.condicional.exercicio;
+
+import java.util.Scanner;
+
+public class Lista11PraticaWhile04 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numeros = sc.nextInt();
+        while (numeros < 10){
+            numeros++;
+            System.out.println(numeros);
+        }
+    }
+}
