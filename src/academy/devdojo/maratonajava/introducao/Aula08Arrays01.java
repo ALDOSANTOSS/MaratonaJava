50c5aec376dd422c6b389473e87b96dc16d8da77
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-31 12:17:17 -0300

    Iniciando o Arryas

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08Arrays01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08Arrays01 {
+    public static void main(String[] args) {
+        int [] idade = new int[3];
+        System.out.println(idade[2]);
+    }
+}
+
+
