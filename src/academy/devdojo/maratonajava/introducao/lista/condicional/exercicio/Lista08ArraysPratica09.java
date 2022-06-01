4dbbdb487826004972405e27323c66bca5a020a4
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-01 11:42:40 -0300

    Preciso pratica Arrays um pouco mais

---
+++ src/academy/devdojo/maratonajava/introducao/lista/condicional/exercicio/Lista08ArraysPratica09.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao.lista.condicional.exercicio;
+
+public class Lista08ArraysPratica09 {
+    public static void main(String[] args) {
+        String[] nomes = new String[3];
+        nomes[0]= "Aldo";
+        nomes[1]= "Silva";
+        nomes[2]= "Pedro";
+
+        for(int contador = 0; contador < 3; contador++){
+            System.out.println(nomes[contador]);
+        }
+
+    }
+
+}
+
