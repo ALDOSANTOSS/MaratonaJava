c08f2a15a6c8025b890ba9fa1cf5cd5cb65f64ed
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-03 17:23:17 -0300

    Treino

---
+++ src/academy/devdojo/maratonajava/introducao/lista/condicional/exercicio/Lista08ArraysPratica11.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao.lista.condicional.exercicio;
+
+public class Lista08ArraysPratica11 {
+    public static void main(String[] args) {
+        String [] pessoas = new String[10];
+        pessoas [1] = "Pedro";
+        System.out.println(pessoas[1]);
+    }
+}
+
---
+++ src/academy/devdojo/maratonajava/introducao/lista/condicional/exercicio/Lista08Pratica10.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.introducao.lista.condicional.exercicio;
+// 1) Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor
+//que C.
+
+import java.util.Scanner;
+
+public class Lista08Pratica10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = a + b;
+
+        if (c < d) {
+            System.out.println("verdade");
+        } else {
+            System.out.println("Mentira");
+        }
+
+
+    }
+}
