6547a0a7413bd8589ce345500986167c8acd08f6
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-03 22:33:50 -0300

    loops

---
+++ src/academy/devdojo/maratonajava/introducao/lista/condicional/exercicio/Lista11PraticaFor06.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao.lista.condicional.exercicio;
+
+
+public class Lista11PraticaFor06 {
+    public static void main(String[] args) {
+
+
+        for (int pessoas = 0; pessoas < 10; pessoas++) {
+            System.out.println(pessoas);
+        }
+
+    }
+
+}
