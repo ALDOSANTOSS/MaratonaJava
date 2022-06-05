e157de6a8ad14313f9a6667a36be25816368985a
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-05 12:56:12 -0300

    Treinos de imc

--- src/academy/devdojo/maratonajava/introducao/Aula08Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08Arrays01.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula08Arrays01 {
     public static void main(String[] args) {
         int [] idade = new int[3];
+        idade[0]= 1;
         System.out.println(idade[2]);
     }
 }
