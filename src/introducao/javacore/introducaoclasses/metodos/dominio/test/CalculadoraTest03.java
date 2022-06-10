881092c1e5e6c26fc8683ff574e95f616280acda
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-10 20:18:03 -0300

    Revisando Orientacao

---
+++ src/introducao/javacore/introducaoclasses/metodos/dominio/test/CalculadoraTest03.java
@@ -0,0 +1,10 @@
+package introducao.javacore.introducaoclasses.metodos.dominio.test;
+
+import introducao.javacore.introducaoclasses.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.diviveDoisNumeros(20,10);
+    }
+}
