90663842f023bdaf6edf57643ea73421db640dfb
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-09 21:42:47 -0300

    Revisando for/ doente

---
+++ src/academy/devdojo/maratonajava/lista/condicional/exercicio/Lista11PraticaFor08.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.lista.condicional.exercicio;
+
+public class Lista11PraticaFor08 {
+
+    public static void main(String[] args) {
+        for(int pessoa = 0; pessoa <100; pessoa++){
+            System.out.println(pessoa);
+        }
+    }
+}
